0ad8312e0c029dc7d4be36ac01b4cce447d42cb3
Simon Hanna <simon.a.hanna93@gmail.com>
2015-10-24 23:01:03 -0600

    Changed index view to route to account information
    Will need to be adjusted after we create accounts

--- website/views.py
+++ website/views.py
@@ -5,11 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request, 'website/index.html');
+	return render(request, 'website/account_info.html');
 
 def purchase_history(request):
 	return render(request, 'website/purchase_history.html');
 
 def account_info(request):
 	return render(request, 'website/account_info.html');
-
+	
