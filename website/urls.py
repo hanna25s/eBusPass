cd007e9f93b28fd5bf25f09aba20b0cdc8011bd2
RockyKelvin <77065462@qq.com>
2015-11-01 12:41:10 -0600

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     url(r'^registration/', views.registration),
     url(r'^signin/', views.signin),
     url(r'^user_profile/', views.user_profile),
+    url(r'^landing_page/', views.landing_page),
 
 ]
