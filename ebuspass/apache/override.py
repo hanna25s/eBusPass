f0e562c6f4a333d11060d5114bc18c8d00dbc4a8
Simon Hanna <simon.a.hanna93@gmail.com>
2016-02-09 19:41:25 -0600

    Adding SSL to allowed hosts

--- ebuspass/apache/override.py
+++ ebuspass/apache/override.py
@@ -4,4 +4,6 @@ from ebuspass.settings import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://52.73.115.239/', '52.73.115.239', 'ec2-54-84-253-83.compute-1.amazonaws.com', 'http://www.ebuspass.com']
+ALLOWED_HOSTS = ['http://52.73.115.239/', 'https://52.73.115.239/', '52.73.115.239',
+                'ec2-54-84-253-83.compute-1.amazonaws.com', 'http://www.ebuspass.com',
+                'https://www.ebuspass.com']
